2d57b30d4748b2c0f9936abb5ebbf0283c348076
Tane Piper <piper.tane@gmail.com>
2012-06-16 22:22:32 +0100

    Much nicer interface

--- paths/app/index.js
+++ paths/app/index.js
@@ -1,9 +1,30 @@
 module.exports = function(namespace, dnode, instance, client, connection) {
 
+  var _ = require('underscore');
   var twitter = require('ntwitter');
   var request = require('request');
+  var moment = require('moment');
 
   dnode[namespace] = {};
+
+
+  dnode[namespace].retweet = function(id, cb) {
+    console.log('Retweet request', id);
+    dnode.session(function(err, session) {
+      if (err) {
+        return cb(err);
+      }
+      var twit = new twitter({
+        consumer_key: instance.options.oauth.consumer_key,
+        consumer_secret: instance.options.oauth.consumer_secret,
+        access_token_key: session.oauth.access_token,
+        access_token_secret: session.oauth.access_token_secret
+      });
+      twit.retweetStatus(id, cb);
+    });
+  };
+
+  /*
 
   dnode[namespace].search = function(term, params, cb) {
     if (typeof params === 'function') {
@@ -51,7 +72,21 @@ module.exports = function(namespace, dnode, instance, client, connection) {
         access_token_key: session.oauth.access_token,
         access_token_secret: session.oauth.access_token_secret
       });
-      twit.verifyCredentials(function (err, data) {}).getUserTimeline(cb);
+      twit.verifyCredentials(function (err, data) {
+        if (err) {
+          return cb(err);
+        }
+        twit.getUserTimeline(function(data) {
+          var tweet = {
+            tweet_id: message.id_str,
+            screen_name: message.user.screen_name,
+            profile_image: message.user.profile_image_url,
+            date: moment(message.created_at).format("MMM Do YYYY, hh:mm:ss"),
+            text: message.text
+          };
+          cb(null, tweet);
+        });
+      });
     });
   };
 
@@ -74,11 +109,34 @@ module.exports = function(namespace, dnode, instance, client, connection) {
         access_token_key: session.oauth.access_token,
         access_token_secret: session.oauth.access_token_secret
       });
-      twit.verifyCredentials(function (err, data) {}).getHomeTimeline(options, cb);
+
+      twit.verifyCredentials(function (err, data) {
+        if (err) {
+          return cb(err);
+        }
+        twit.getHomeTimeline(function(data) {
+          var tweet = {
+            tweet_id: message.id_str,
+            screen_name: message.user.screen_name,
+            profile_image: message.user.profile_image_url,
+            date: moment(message.created_at).format("MMM Do YYYY, hh:mm:ss"),
+            text: message.text
+          };
+          cb(null, tweet);
+        });
+      });
     });
   };
+   */
 
   dnode[namespace].sendTweet = function(options, cb) {
+    if (!options || !options.status) {
+      return cb('You must pass a status to sendTweet');
+    }
+
+    _.extend(options, {
+      'include_entities': true
+    });
 
     dnode.session(function(err, session) {
       if (err) {
@@ -95,6 +153,7 @@ module.exports = function(namespace, dnode, instance, client, connection) {
     });
   };
 
+  /*
   dnode[namespace].getDirectMessages = function(params, cb) {
     if (typeof params === 'function') {
       cb = params;
@@ -116,7 +175,7 @@ module.exports = function(namespace, dnode, instance, client, connection) {
       });
       twit.verifyCredentials(function (err, data) {}).getDirectMessages(cb);
     });
-  }
+  };
 
   dnode[namespace].getTweetLocation = function(coordinates, cb) {
     request({
@@ -128,13 +187,68 @@ module.exports = function(namespace, dnode, instance, client, connection) {
       json: true
     }, cb);
   };
+   */
 
-  connection.on('ready', function() {
-    dnode.session(function(err, session) {
-      if (err) {
-        return client.incomingError(err);
+  /**
+   * This is our function to generate the view data
+   */
+  var incomingMessage = function(message) {
+    var output;
+
+    if (message.friends) {
+      this.incomingMessage(null, null);
+    } else {
+      //console.log('Incoming Message', message);
+      output = {
+        type: 'tweet',
+        id: message.id,
+        tweet_id: message.id_str,
+        date: moment(message.created_at).format("MMM Do YYYY, hh:mm:ss"),
+        user: _.extend({}, message.user),
+        entities: _.extend({}, message.entities)
+      };
+
+      var text = message.text.replace(message.user.screen_name, '@' + message.user.screen_name, 'gi');
+
+
+      if (message.entities && message.entities.urls && message.entities.urls.length > 0) {
+        message.entities.urls.forEach(function(url){
+          text = text.replace(url.url, '<a href="' + url.expanded_url + '" title="' + url.expanded_url + '">' + url.display_url + '</a>', 'gi');
+          output.entities.urls.push(url);
+        });
       }
 
+      if (message.entities && message.entities.hashtags && message.entities.hashtags.length > 0) {
+        message.entities.hashtags.forEach(function(hashtag){
+          text = text.replace('#' + hashtag.text, '<a href="/search/' + encodeURIComponent('#' + hashtag.text) + '" title="' + '#' + hashtag.text + '">' + '#' + hashtag.text + '</a>', 'gi');
+          output.entities.hashtags.push(hashtag);
+        });
+      }
+
+      if (message.entities && message.entities.user_mentions && message.entities.user_mentions.length > 0) {
+        message.entities.user_mentions.forEach(function(user_mention){
+          text = text.replace('@' + user_mention.screen_name, '<a href="/search/' + encodeURIComponent('@' + user_mention.screen_name) + '" title="' + '@' + user_mention.screen_name + '">' + '@' + user_mention.screen_name + '</a>', 'gi');
+          output.entities.user_mentions.push(user_mention);
+        });
+      }
+
+      output.text = text;
+
+      this.incomingMessage(null, output);
+    }
+  };
+
+  // Now we know the client and server have a connection
+  connection.on('ready', function() {
+    // Get our session data
+    dnode.session(function(err, session) {
+
+
+      if (err) {
+        return client.incomingError('Connection Ready Error in Session', err);
+      }
+
+      // If we already have Oauth data in the session then we don't need to get it from the user
       if (session.oauth) {
         var twit = new twitter({
           consumer_key: instance.options.oauth.consumer_key,
@@ -153,11 +267,13 @@ module.exports = function(namespace, dnode, instance, client, connection) {
         });
         */
 
+        /**
+         * Connect to the user stream
+         */
         twit.stream('user', function(stream) {
-          stream.on('data', function(data) {
-            console.log('data', data);
-            client.incomingMessage(data);
-          });
+
+          // When data comes in pass to incoming Message
+          stream.on('data', incomingMessage.bind(client));
 
           stream.on('error', function(error) {
             console.log('error', error);
--- static/js/app/Tweetifies.js
+++ static/js/app/Tweetifies.js
@@ -15,10 +15,110 @@ _.extend(Tweetifies, {
     views: {},
     collections: {},
     models: {},
-    templates: {}
+    templates: {},
+    messages: {}
   },
+
+  geocode_status: false,
+  geocode_watcher: null,
+  current_position: null,
+
+  onPositionUpdate: function(position) {
+    console.log(position);
+  },
+
+  onPositionError: function(error) {
+    console.log(error);
+  },
+
   init: function() {
     Tweetifies.loadDnode();
+
+    $('#tweet-text').on('keyup', function(e) {
+      var len = $(this).val().length;
+      var left = 140 - len;
+      $('.chars-left').text(left);
+    });
+
+    $('.geocode').on('click', function() {
+      console.log($(this).hasClass('on'));
+
+      if ($(this).hasClass('on')) {
+        Tweetifies.geocode_status = true;
+        Tweetifies.geocode_watcher = navigator.geolocation.watchPosition(Tweetifies.onPositionUpdate, Tweetifies.onPositionError, {enableHighAccuracy:true});
+
+        navigator.geolocation.getCurrentPosition(function(position) {
+          Tweetifies.current_position = position.coords;
+        });
+
+        $('.geocode.off').removeClass('active');
+        $(this).addClass('active');
+
+      } else {
+        Tweetifies.geocode_status = false;
+        navigator.geolocation.clearWatch(Tweetifies.geocode_watcher);
+
+        $('.geocode.on').removeClass('active');
+        $(this).addClass('active');
+      }
+    });
+
+    $('#twitter-input a.tweet').on('click', function(e) {
+      e.preventDefault();
+
+      var value = $('#tweet-text').val();
+      if (value.length > 0 && value.length <= 140) {
+        var to_send = {
+          status: value
+        };
+        if (Tweetifies.geocode_status) {
+          to_send.lat = Tweetifies.current_position.latitude;
+          to_send.long = Tweetifies.current_position.longitude;
+        }
+
+        $('#tweet-text').attr('disabled', true);
+        Tweetifies.remote.app.sendTweet(to_send, function(err, tweet) {
+          if (err) {
+            $('#alerts')
+              .removeClass('alert-success alert-info alert-warning')
+              .addClass('alert-error')
+              .html('<i class="icon-remove-sign"></i> <strong>Uh oh...</strong>' + err);
+          } else {
+            $('#tweet-text').val('');
+          }
+          $('#tweet-text').attr('disabled', false);
+        });
+      } else {
+        $('#alerts')
+          .removeClass('alert-success alert-info alert-warning')
+          .addClass('alert-error')
+          .html('<i class="icon-remove-sign"></i> <strong>Uh oh...</strong>You may only enter text between 1 and 140 characters');
+      }
+    });
+  },
+
+  loadtemplate: function(tpl_name, cb) {
+    if (Tweetifies._cache.templates[tpl_name]) {
+      cb( Tweetifies._cache.templates[tpl_name] );
+    } else {
+      $.get('templates/' + tpl_name + '.html', function(data) {
+        Tweetifies._cache.templates[tpl_name] = _.template($(data).html());
+        cb( Tweetifies._cache.templates[tpl_name] );
+      });
+    }
+  },
+
+  loadMessage: function(id, cb) {
+    var message = Tweetifies._cache.messages[id];
+    if (!message) {
+      cb('Message with id ' + id.toString() + ' does not exist');
+    } else {
+      cb(null, message);
+    }
+  },
+
+  saveMessage: function(id, message) {
+    Tweetifies._cache.messages[id] = message;
   },
 
   loadDnode: function() {
@@ -33,42 +133,116 @@ _.extend(Tweetifies, {
     });
   },
 
-  incomingMessage: function(message) {
-    console.log('Incoming Message', message);
-
-    // For now we're going to assume we have a user object - we'll look at other cases later
-    if (message.user) {
-      var tpl_string = [
-        '<div class="tweet well span5" id="tweet-' + message.id_str +'" style="display:none; position:relative; style: padding: 10px;">',
-          '<header>',
-            '<h5 style="padding-left: 5px; border-bottom: 2px solid  black; margin-bottom: 5px;">',
-              '<a target="_new" href="http://twitter.com/' + message.user.screen_name + '">@' + message.user.screen_name + '</a>',
-            '</h5>',
-          '</header>',
-          '<section class="main" style="position:relative;">',
-            '<div style="float:left; position:absolute; top: 0; left: 0; width:50px; height: 80px">',
-              '<img src="' + message.user.profile_image_url + '" style="margin-left: 5px;"/>',
-            '</div>',
-            '<div style="float:left; position:absolute; top: 0; left: 50px; width: 150px; height: 80px;">',
-              'Sent <a href="">' + moment(message.created_at).format("MMM Do 'YY, hh:mm:ss") + '</a>',
-            '</div>',
-            '<div style="float:left; position:absolute; top: 0; left: 200px; width: 250px; height: 80px;">',
-              '<p>' + message.text + '</p>',
-            '</div>',
-          '</section>',
-          '<footer style="clear:both;">',
-          '</footer>',
-        '</div>'
-      ].join('');
-
-
-      $('#twitter-output').append(tpl_string);
-
-      $('#tweet-' + message.id_str).fadeIn();
+  incomingMessage: function(err, message) {
+    if (!message || message.friends) {
+      console.log('Message Skipped');
+      return;
     }
+
+    Tweetifies.saveMessage(message.id, message);
+
+    Tweetifies.loadtemplate('tweet', function(tpl) {
+      $('#twitter-output').prepend(tpl(message));
+
+      $('.brand', '#tweet-' + message.id).popover({
+        placement: 'bottom',
+        title: '@' + message.user.screen_name,
+        content: [
+          '<img class="pull-left" src="' + message.user.profile_image_url + '" />',
+          '<div class="pull-left">',
+            '<h6>' + message.user.name + '</h6>',
+            ( (message.user.description) ? '<p>' + message.user.description + '</p>' : '' ),
+          '</div>',
+          '<div style="clear: both;"></div>'
+        ].join('')
+      });
+
+      $('#tweet-' + message.id)
+      .find('a').on('click', Tweetifies.handleButton)
+      .end()
+      .fadeIn(1000);
+    });
   },
 
   incomingError: function(error) {
     console.error('Incoming Error', error);
+  },
+
+  handleButton: function(e) {
+    e.preventDefault();
+
+    var el = this;
+
+    var tweet_container = $(this).closest('.tweet');
+    var id = $(tweet_container).data('tweet');
+
+    var output;
+    Tweetifies.loadMessage(id, function(err, message) {
+      console.log('Message Loaded', err, message);
+      if (err) {
+        return Tweetifies.incomingError(message);
+      }
+
+      var action_clicked = $(el).attr('class');
+
+      var action_clicked_text = $(this).text();
+
+      if (action_clicked === 'reply') {
+        output = [];
+        if (message.entities.user_mentions.length > 0) {
+          message.entities.user_mentions.forEach(function(mention) {
+            if (output.indexOf('@' + mention.screen_name + ' ') === -1) {
+              output.push('@' + mention.screen_name + ' ');
+            }
+          });
+        }
+        output.push('@' + message.user.screen_name + ' ');
+        $('#tweet-text').val(output.join(''));
+
+        $('#tweet-text').focus(function() {
+          var end = $('#tweet-text').val().length;
+          $('#tweet-text').selectionStart = $('#tweet-text').selectionEnd = end;
+        });
+
+      } else if (action_clicked === 'direct-message') {
+        output = [];
+        output.push('d ' + message.user.screen_name + ' ');
+        $('#tweet-text').val(output.join(''));
+      } else if (action_clicked === 'retweet') {
+        var c = confirm('Retweet to your followers?');
+        if (c) {
+          Tweetifies.remote.app.retweet(message.tweet_id, function() {
+            console.log(arguments);
+          });
+        }
+      }
+    });
+  },
+    /*
+    e.preventDefault();
+    var id = $(this).attr('href');
+    var tweet = $(id);
+    var footer = $('footer', tweet);
+
+    console.log(id, tweet, footer);
+
+    return this;
+     */
+
+  buttonRetweet: function(e) {
+    /*
+    e.preventDefault();
+    var id = $(this).attr('href').split('-')[1];
+    var tweet = $($(this).attr('href'));
+    var footer = $('footer', tweet);
+
+    console.log(id, tweet, footer);
+
+    Tweetifies.remote.app.retweet(id, function(err, data) {
+      console.log(arguments);
+    });
+
+    return this;
+     */
   }
 });
