4082a170a412a284c7f944da82508db6857c5eeb
Tane Piper <piper.tane@gmail.com>
2012-06-23 04:04:05 +0100

    Small cleanup

--- server/app/index.js
+++ server/app/index.js
@@ -3,7 +3,6 @@ var shoe = require('shoe');
 module.exports = function(instance) {
 
   var sock = shoe(require('./connection')(instance));
-  //sock.on('connection', require('./connection')(instance));
 
   sock.install(instance.express, { prefix : '/tweetifies' });
 
