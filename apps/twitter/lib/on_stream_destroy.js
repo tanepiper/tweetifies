5f6f4d700813fe8076e9412a1d6148a892d964a0
Tane Piper <piper.tane@gmail.com>
2012-06-24 01:45:51 +0100

    More cruft removed

--- apps/twitter/lib/on_stream_destroy.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(instance, user, remote) {
-
-  return function(destroy) {
-    console.log('============== destroy event ==============');
-    console.log(destroy);
-    console.log('============ end destroy event ============');
-  };
-};
--- apps/twitter/lib/on_stream_error.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(instance, user, remote) {
-
-  return function(error) {
-    console.log('============== error event ==============');
-    console.log(error);
-    console.log('============ end error event ============');
-  };
-};
